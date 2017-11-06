5f4ae92b687ad18e36a74ede3e67217a1a5a329a
ulanzetz <ulanzet@gmail.com>
2017-11-06 11:53:15 +0500

    Added mutihthreading in server

--- server/Server.java
+++ server/Server.java
@@ -1,25 +1,22 @@
 package com.snakegame.server;
 
-import com.snakegame.model.Board;
 import com.snakegame.model.Game;
 import com.snakegame.model.GameMode;
-import com.snakegame.model.Snake;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.HashMap;
 
+import static java.lang.Integer.getInteger;
 import static java.lang.Integer.parseInt;
 
 public class Server {
+
     public static void main(String args[]) throws Exception {
-        int serverPort = parseInt(args[0]);
+        int serverStartPort = parseInt(args[0]);
         int width = parseInt(args[1]);
         int height = parseInt(args[2]);
         if(width < 3 || height < 3)
@@ -29,34 +26,59 @@ public class Server {
         GameMode mode = GameMode.gameMods.get(args[4]);
         Game game = new Game(width, height, delay, mode, null, null);
 
-        HashMap<InetAddress, Integer> playersIDs = new HashMap<InetAddress, Integer>();
-        DatagramSocket socket = new DatagramSocket(serverPort);
+        for(int i = 0; i != game.gameMode.snakeCount; ++i) {
+            DatagramSocket socket = new DatagramSocket(serverStartPort + i);
+            new Thread(new Responder(socket, game, args[4], i)).start();
+        }
+    }
+
+}
+class Responder implements Runnable {
+    private DatagramSocket socket = null;
+    private Game game;
+    private static HashMap<String, Integer> playersIDs = new HashMap<>();
+    private int connectedPlayers = 0;
+    private String modeName;
+    private int playerID;
+
+    public Responder(DatagramSocket socket, Game game, String modeName, int playerID) {
+        this.socket = socket;
+        this.game = game;
+        this.modeName = modeName;
+        this.playerID = playerID;
+    }
+    public void run() {
         byte[] receiveData = new byte[10];
         byte[] sendData;
-        int connectedPlayers = 0;
-
         while (true) {
             DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
-            socket.receive(packet);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             String data = new String(packet.getData());
             InetAddress ip = packet.getAddress();
             int clientPort = packet.getPort();
+            String ipPort = ip.toString() + ":" + clientPort;
             if(data.startsWith("con")) {
-                if(connectedPlayers == mode.snakeCount) {
+                if(connectedPlayers == game.gameMode.snakeCount) {
                     sendData = "not".getBytes();
                 }
                 else {
-                    if(!playersIDs.containsKey(ip))
-                        playersIDs.put(ip, connectedPlayers++);
-                    sendData = (args[1] + " " + args[2] + " " + args[3] + " " + playersIDs.get(ip) + " " +
-                             args[4] + " ").getBytes();
+                    if(!playersIDs.containsKey(ipPort))
+                        playersIDs.put(ipPort, connectedPlayers++);
+                    sendData = (game.board.getWidth() + " " +
+                            game.board.getHeight() + " " +
+                            game.delay  + " " +
+                            playersIDs.get(ipPort) + " " +
+                            modeName + " ").getBytes();
                 }
             }
             else {
                 String[] splitedData = data.split(" ");
                 int x = parseInt(splitedData[0]);
                 int y = parseInt(splitedData[1]);
-                int playerID = playersIDs.get(ip);
                 game.board.snakes[playerID].setDirection(new Point(x, y));
                 String mes = "";
                 for(int i = 0; i != game.gameMode.snakeCount; ++i) {
@@ -68,8 +90,13 @@ public class Server {
                 sendData = mes.getBytes();
             }
             packet = new DatagramPacket(sendData, sendData.length, ip, clientPort);
-            socket.send(packet);
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
     }
 
 }
